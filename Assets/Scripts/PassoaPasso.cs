01896394ca1520a5a20a75c9fc4b2b1cad8ca276
Gustavo Hochgraf <gusthoch@gmail.com>
2021-09-06 01:31:37 -0300

    botão next e previous funcionando em conjunto com textos de guia

--- Assets/Scripts/PassoaPasso.cs
+++ Assets/Scripts/PassoaPasso.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
@@ -13,24 +15,66 @@ public class PassoaPasso : MonoBehaviour
     //[SerializeField]
     //private Animator baseGarraAnim;
     [SerializeField]
-    private 
+    private int maxiAnim;
+    [SerializeField]
+    private TextAsset textFile;
+    [SerializeField]
+    TextMeshProUGUI painelTextoGuia;
+
+    private int contadorAnimacoes = 0;
+    List<string> textos = new List<string>();
+    private bool inicio;
 
     void Awake()
     {
+        leitorCSV(textFile);
+        painelTextoGuia.text = "Posicione o QR Code sobre uma superfície plana e aponte a câmera para ele";
         btnProximo.onClick.AddListener(Proximo);
         btnAnterior.onClick.AddListener(Anterior);
     }
+    private void Update()
+    {
+        if (!FindObjectOfType<ARTrackedImage>())
+        {
+            inicio = true;
+            painelTextoGuia.text = "Posicione o QR Code sobre uma superfície plana e aponte a câmera para ele";
+        }else if ((FindObjectOfType<ARTrackedImage>()) && (inicio==true))
+        {
+            painelTextoGuia.text = textos[contadorAnimacoes];
+            inicio = false;
+        }
+
+    }
     private void Proximo()
     {
-        ARTrackedImage trackedimage = FindObjectOfType<ARTrackedImage>();
-        Animator animator = trackedimage.GetComponent<Animator>();
-        animator.SetBool("anim1bool", true);
+        if (contadorAnimacoes != maxiAnim)
+        {
+            contadorAnimacoes++;
+            ARTrackedImage trackedimage = FindObjectOfType<ARTrackedImage>();
+            Animator animator = trackedimage.GetComponent<Animator>();
+            animator.SetInteger("animCounter", contadorAnimacoes);
+            painelTextoGuia.text = textos[contadorAnimacoes];
+        }
     }
     private void Anterior()
     {
-        ARTrackedImage trackedimage = FindObjectOfType<ARTrackedImage>();
-        Animator animator = trackedimage.GetComponent<Animator>();
-        animator.SetBool("anim1bool", false);
+        if (contadorAnimacoes != 0)
+        {
+            contadorAnimacoes--;
+            ARTrackedImage trackedimage = FindObjectOfType<ARTrackedImage>();
+            Animator animator = trackedimage.GetComponent<Animator>();
+            animator.SetInteger("animCounter", contadorAnimacoes);
+            painelTextoGuia.text = textos[contadorAnimacoes];
+        }
     }
 
+    private void leitorCSV(TextAsset arquivoTexto)
+    {
+        var lines = arquivoTexto.text.Split("\r\n"[0]);
+        foreach (var linha in lines)
+        {
+            linha.Replace("\n", "");
+            textos.Add(linha);
+        }
+    }
 }
